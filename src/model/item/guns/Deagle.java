add1d26d4fc379e8565ecf384215d87d0d9c2bed
dkwapisz <dkwapisz@student.agh.edu.pl>
2021-07-29 10:48:14 +0200

    Final

---
+++ src/model/item/guns/Deagle.java
@@ -0,0 +1,13 @@
+package model.item.guns;
+
+import javafx.scene.layout.Pane;
+
+public class Deagle extends Gun {
+
+    public Deagle(double x, double y, Pane layer) {
+        super(x, y, null, null, layer);
+        // ***** Zadanie 3 *****
+    }
+
+}
+
